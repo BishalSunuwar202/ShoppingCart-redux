11558315fc8af8ef3f34de35488ee73c7b9685e9
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2023-09-12 13:28:30 +0545

    component and action creator(thunk)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
-
-//it is created outside of component function for not to reinitize while rerender  
-//it is also used to solve a problem which is 
+//it is created outside of component function for not to reinitize while rerender
+//it is also used to solve a problem which is
 //at first fetch, empty cart will be sent to backend, which can be seen in network
 //so we will not sent data at first initilization but sent after first
 let isInitial = true;
-
 
 function App() {
   const dispatch = useDispatch();
@@ -22,57 +20,53 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "sending",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://food-delivery-redux-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        //throw new Error("Sending cart data failed");
-        dispatch(
-          uiActions.showNotification({
-            status: "error",
-            title: "Error",
-            message: "Error Sending cart data!",
-          })
-        );
-      }
+    // const sendCartData = async () => {
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sending cart data successfully!",
-        })
-      );
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "pending",
+    //     title: "sending",
+    //     message: "Sending cart data!",
+    //   })
+    // );
+    // const response = await fetch(
+    //   "https://food-delivery-redux-default-rtdb.firebaseio.com/cart.json",
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify(cart),
+    //   }
+    // );
+    // if (!response.ok) {
+    //   throw new Error("Sending cart data failed");
+    //
+    // }
 
-      // const responseData = await response.json();
-    };
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "success",
+    //     title: "Success",
+    //     message: "Sending cart data successfully!",
+    //   })
+    // );
+
+    // const responseData = await response.json();
+    // };
 
     if (isInitial) {
-      isInitial = false
+      isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Error Sending cart data!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
+    // sendCartData().catch((error) => {
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "error",
+    //     title: "Error",
+    //     message: "Error Sending cart data!",
+    //   })
+    // );
+    // });
   }, [cart, dispatch]);
 
   return (
